bd6ff74cce6852d037ece33293242eae2ce4fff4
Igor Lira <igorlirap@gmail.com>
2022-09-13 20:45:40 -0300

    add assets and set background as a component

--- App.tsx
+++ App.tsx
@@ -1,32 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Background } from "./src/components/background";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>NLW e-sport</Text>
-      <StatusBar style="auto" backgroundColor="black" />
-      <Button title="Hello World"></Button>
-    </View>
-  );
+  return <Background></Background>;
 }
-
-interface ButtonProps {
-  title: String;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  );
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
